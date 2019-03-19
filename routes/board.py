7f2c723a1a6395d58e9e01380b791a2e9724addc
Janson <princecx@126.com>
2019-03-19 21:29:03 +0800

    finish work changes

--- routes/board.py
+++ routes/board.py
@@ -16,14 +16,32 @@ main = Blueprint('board', __name__)
 
 @main.route("/admin")
 def index():
-    return render_template('board/admin_index.html')
-    ...
+    u = current_user()
+    boards = Board.all()
+
+    if u.role == 1:
+        return render_template('board/admin_index.html', boards=boards)
+    else:
+        return redirect(url_for('topic.index'))
 
 
 @main.route("/add", methods=["POST"])
 def add():
-    form = request.form
     u = current_user()
-    m = Board.new(form)
-    return redirect(url_for('topic.index'))
+    if u.role == 1:
+        form = request.form
+        u = current_user()
+        m = Board.new(form)
+        return redirect(url_for('topic.index'))
+    else:
 
+        return redirect(url_for('topic.index'))
+
+
+@main.route("/delete")
+def delete():
+    u = current_user()
+    board_id = int(request.args.get("board_id"))
+    if u.role == 1:
+        Board.delete(board_id)
+        return redirect(url_for('.index'))
